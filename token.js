cb9c65109bb3278d0c7f32cf426f465578a1d6d5
XpatzX <bravepaattt206@gmail.com>
2026-03-14 14:34:56 +0800

    Update token.js with new token and remove old one

--- token.js
+++ token.js
@@ -1,6 +1,6 @@
 // liat apa
 module.exports = [
   "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
-  "6789012345:ABCdefGHIjklmNOPqrstUVwxy1",
+  "8751488346:AAFEz7A4RjTvkVwf3aslP9_8KNhx6JN6UXo",
   "6789012345:ABCdefGHIjklmNOPqrstUVwxy2"
 ];

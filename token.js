9b7e164439173c4e9ca2af805b95012935461757
XpatzX <bravepaattt206@gmail.com>
2026-03-14 14:33:46 +0800

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,6 @@
 // liat apa
 module.exports = [
-  "6789012345:ABCdefGHIjklmNOPqrstUVwxyz",
+  "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
   "6789012345:ABCdefGHIjklmNOPqrstUVwxy1",
   "6789012345:ABCdefGHIjklmNOPqrstUVwxy2"
 ];

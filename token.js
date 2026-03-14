af71005c4c5fee550bc593809cbabefd87979c60
XpatzX <bravepaattt206@gmail.com>
2026-03-14 14:59:35 +0800

    Update token.js

--- token.js
+++ token.js
@@ -2,5 +2,6 @@
 module.exports = [
   "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
   "8751488346:AAFEz7A4RjTvkVwf3aslP9_8KNhx6JN6UXo",
-  "8289381593:AAH9G0hqk9Ri9dla07UzeJtH1GtaEnVbPmw"
+  "8289381593:AAH9G0hqk9Ri9dla07UzeJtH1GtaEnVbPmw",
+  "8468849511:AAHNxjegXCnunjrWlZn-A0QzwsgFtjoCrzI"
 ];

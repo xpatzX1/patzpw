a384f0425c5481834892e910f7e6a99020808fbe
XpatzX <bravepaattt206@gmail.com>
2026-03-14 15:06:43 +0800

    Add new token to token.js

--- token.js
+++ token.js
@@ -3,5 +3,6 @@ module.exports = [
   "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
   "8751488346:AAFEz7A4RjTvkVwf3aslP9_8KNhx6JN6UXo",
   "8289381593:AAH9G0hqk9Ri9dla07UzeJtH1GtaEnVbPmw",
-  "8468849511:AAHNxjegXCnunjrWlZn-A0QzwsgFtjoCrzI"
+  "8468849511:AAHNxjegXCnunjrWlZn-A0QzwsgFtjoCrzI",
+  "8662614677:AAFEcD3TeGNWhC8Ix9la-rP0DGO24C87t3c"
 ];

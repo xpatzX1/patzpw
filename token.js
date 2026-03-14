847edbf2f1e28c6b84b17c2f68cee9e3f40e189f
XpatzX <bravepaattt206@gmail.com>
2026-03-14 14:54:22 +0800

    Replace token in token.js

--- token.js
+++ token.js
@@ -2,5 +2,5 @@
 module.exports = [
   "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
   "8751488346:AAFEz7A4RjTvkVwf3aslP9_8KNhx6JN6UXo",
-  "6789012345:ABCdefGHIjklmNOPqrstUVwxy2"
+  "8289381593:AAH9G0hqk9Ri9dla07UzeJtH1GtaEnVbPmw"
 ];

cabbeedcbab90ee45c07a47ea58f2b5d31fb41fc
XpatzX <bravepaattt206@gmail.com>
2026-03-18 13:55:24 +0800

    Update token in token.js

--- token.js
+++ token.js
@@ -1,6 +1,6 @@
 // X. apa nengok
 module.exports = [
-  "7673184496:AAGP15rymZxu-dr-Z0iDekNGOjoJZeEyWgo",
+  "8799276855:AAGsVgrJ47_9xJ2doIOby6lgIqPkOoc-ny8",
   "8751488346:AAFEz7A4RjTvkVwf3aslP9_8KNhx6JN6UXo",
   "8289381593:AAH9G0hqk9Ri9dla07UzeJtH1GtaEnVbPmw",
   "8468849511:AAHNxjegXCnunjrWlZn-A0QzwsgFtjoCrzI",
